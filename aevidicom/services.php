8c614167cdcd8b57011df954c8efd33d837b0ecd
Retricide <eric.segev@gmail.com>
2013-06-30 13:11:43 -0400

    Added aevidicom folder
    For aevidi.com site development

---
+++ aevidicom/services.php
@@ -0,0 +1,288 @@
+<html lang="en" class=" js no-touch svg inlinesvg svgclippaths no-ie8compat" style="">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width" name="viewport">
+  <title>aeVidi LLC | Seeing is Believing</title>
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/foundation.css" rel="stylesheet" type="text/css">
+  <link href="css/custom.css" rel="stylesheet" type="text/css">
+  <link href="css/fc-webicons.css" rel="stylesheet" type="text/css">
+  <script src="js/vendor/custom.modernizr.js" type="text/javascript">
+</script>
+</head>
+
+<body>
+
+<a class="scrollup" href="#"></a>
+
+<div class="hidden no-margin" id="home"></div>
+
+<div id="contact-modal" class="reveal-modal">
+		<h4>Contact us</h4>
+		<div class="row flat panel contact-panel">
+		  <div class="section-container tabs flat" data-section="">
+		    <section class="section">
+		      <h5 class="title"><a href="#contact-form">Contact Our Company</a></h5>
+		      <div class="content" data-slug="contact-form">
+		        <form method="post" target="">
+		          <div class="row collapse">
+		            <div class="large-2 columns">
+		              <label class="inline">Your Name</label>
+		            </div>
+		            <div class="large-10 columns">
+		              <input type="text" id="yourName" placeholder="Your Name">
+		            </div>
+		          </div>
+							<div class="row collapse">
+		            <div class="large-2 columns">
+		              <label class="inline">Your Phone Number (optional)</label>
+		            </div>
+		            <div class="large-10 columns">
+		              <input type="text" id="yourNumber" placeholder="0000000000">
+		            </div>
+		          </div>
+		          <div class="row collapse">
+		            <div class="large-2 columns">
+		              <label class="inline">Your Email</label>
+		            </div>
+		            <div class="large-10 columns">
+		              <input type="text" id="yourEmail" placeholder="your@email.com">
+		            </div>
+		          </div>
+							<div class="row collapse">
+		            <div class="large-2 columns">
+		              <label class="inline">Your Message</label>
+		            </div>
+		            <div class="large-10 columns">
+		               <textarea id="yourMessage" rows="6"></textarea>
+		            </div>
+		          </div>
+		          <button type="submit" class="radius button">Submit</button>
+		        </form>
+		      </div>
+		    </section>
+		    <section class="section">
+		      <h5 class="title"><a href="#panel2">Meet our Team</a></h5>
+		      <div class="content" data-slug="panel2">
+		      <div class="row">
+		        <ul class="large-block-grid-3 small-centered columns" style="text-align:center;">
+		          <!-- Float images to sides (each in own row) and add about section for each person -->
+		          <li><a href="mailto:abaradwaj@aevidi.com"><img src="http://placehold.it/200x200&amp;text=[person]"><br>Arjun Baradwaj</a></li>
+		          <li><a href="mailto:sliu@aevidi.com"><img src="http://placehold.it/200x200&amp;text=[person]"><br>Shabai Liu</a></li>
+		          <li><a href="mailto:esegev@aevidi.com"><img src="http://placehold.it/200x200&amp;text=[person]"><br>Eric Segev</a></li>
+		        </ul>
+		      </div>
+		      </div>
+		    </section>
+		  </div>
+	  </div>
+	</div>
+
+  <nav class="row fixed" id="navigation">
+    <div class="large-12 small-centered columns no-pad">
+	    <div class="navbar">
+	      <div class="container">
+	        <a href="index.php"><div class="logo-container large-4 columns block">
+	          <div class="centered large-5 small-5 small-centered"><img class="logo-img-eye" src="img/eye-only.png"></div>
+	          <div class="centered large-5 small-5 small-centered"><img class="logo-img-name" src="img/name-only.png"></div>
+	        </div></a>
+	        <div class="nav-container large-8 columns block">
+	          <ul class="centered nav-ul small-11 mobile-block-grid-2">
+	            <li class="nav-link"><a href="index.php#about-us">ABOUT</a></li>
+	            <li class="nav-link"><a href="portfolio.php">PORTFOLIO</a></li>
+	            <li class="nav-link"><a href="services.php">SERVICES</a></li>
+	            <li class="nav-link"><a class="scroll" href="#contact-us">CONTACT</a></li>
+	            <li class="nav-link"><a class="social-icons" target="_blank" href="https://www.facebook.com/aevidi"><img src="img/fb-round.png" alt="fb-icon" width="32" height="32"></a></li>
+	            <li class="nav-link"><a class="social-icons" href="#"><img src="img/twitter-round.png" alt="tw-icon" width="32" height="32"></a></li>
+							<!-- The process -->
+	          </ul>
+	        </div>
+	      </div>
+      </div>
+    </div>
+    <div class="clear"></div>
+    <div class="background"></div>
+  </nav>
+
+  <div class="page-container">
+
+    <div class="row bottom-border">
+      <div class="large-12 small-centered columns no-pad">
+    	<div class="jumbotron small">
+				<div class="container jumbo-container">
+					<div class="row" style="margin-bottom: 15px;">
+						<span class="hero-s">Portfolio</span>
+					</div>
+				</div>
+    	</div>
+    	</div>
+    </div>
+
+    <div class="row divider bg"></div>
+
+		<div class="row white-band">
+			<div class="container">
+				<ul class="large-12 columns large-block-grid-5 small-block-grid-2 small-centered">
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+					<li><img src="http://placehold.it/100x100&text=[img]"></li>
+				</ul>
+			</div>
+		</div>
+
+    <div class="hidden" id="contact-us"></div>
+
+		<div class="row show-for-large-only vert-space" id="contact-us">
+    	<div class="container">
+				<div class="large-12 columns">
+					<div class="panel">
+						<h4>Get in touch!</h4>
+						<div class="row">
+							<div class="large-9 columns">
+								<p>We'd love to hear from you, you attractive person you. Add information about quotes, etc</p>
+							</div>
+							<div class="large-3 columns">
+								<a href="#contact-modal" data-reveal-id="contact-modal" class="radius button right">Contact Us</a>
+							</div>
+						</div>
+					</div>
+				</div>
+    	</div>
+		</div>
+
+    <div class="row show-for-small-only vert-space" id="contact-us">
+    	<div class="container">
+				<div class="large-12 columns">
+					<div class="panel">
+			      <h4 class="title">Contact Our Company</h4>
+			      <div class="content" data-slug="contact-form">
+			        <form id="contact-form" method="post" target="">
+			          <div class="row collapse">
+			            <div class="large-2 columns">
+			              <label class="inline">Your Name</label>
+			            </div>
+			            <div class="large-10 columns">
+			              <input type="text" id="yourName" placeholder="Your Name">
+			            </div>
+			          </div>
+								<div class="row collapse">
+			            <div class="large-2 columns">
+			              <label class="inline">Your Phone Number (optional)</label>
+			            </div>
+			            <div class="large-10 columns">
+			              <input type="text" id="yourNumber" placeholder="0000000000">
+			            </div>
+			          </div>
+			          <div class="row collapse">
+			            <div class="large-2 columns">
+			              <label class="inline">Your Email</label>
+			            </div>
+			            <div class="large-10 columns">
+			              <input type="text" id="yourEmail" placeholder="your@email.com">
+			            </div>
+			          </div>
+								<div class="row collapse">
+			            <div class="large-2 columns">
+			              <label class="inline">Your Message</label>
+			            </div>
+			            <div class="large-10 columns">
+			               <textarea id="yourMessage" rows="6"></textarea>
+			            </div>
+			          </div>
+			          <button type="submit" class="radius button">Submit</button>
+			        </form>
+			      </div>
+					</div>
+				</div>
+    	</div>
+		</div>
+
+  </div>
+
+  <footer class="row footer" id="footer">
+      <div class="large-12 small-centered columns no-pad">
+		    <div class="container">
+		      <div class="row">
+		        <div class="large-4 columns">
+		          WIDGET TITLE
+		        </div>
+		        <div class="large-4 columns">
+		          WIDGET TITLE
+		        </div>
+		        <div class="large-4 columns">
+		          WIDGET TITLE
+		        </div>
+		      </div>
+		      <div class="row">
+			      <div class="small-centered large-6 tagline columns">
+			        <div class="large-3 columns">
+		          Tagline goes here.
+		        </div>
+			        <div class="large-6 columns"><img src="img/grey-eye.png"></div>
+			        <div class="large-3 columns">
+		          &copy; aeVidi, LLC
+		        </div>
+			      </div>
+		      </div>
+		    </div>
+      </div>
+  </footer>
+
+  <script src="js/vendor/jquery.js" type="text/javascript"></script>
+  <script src="js/foundation.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+		$(document).foundation();
+		$('#contact-modal').on('opened', function () {
+		  $(this).foundation('section', 'reflow');
+		});
+		$(document).ready(function() {
+			$(".scroll").click(function(event){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+			});
+		});
+  </script>
+
+
+
+	<script type="text/javascript">
+	    $(document).ready(function(){
+
+	        $(window).scroll(function(){
+	            if ($(this).scrollTop() > 100) {
+	                $('.scrollup').fadeIn("slow");
+	            } else {
+	                $('.scrollup').fadeOut("slow");
+	            }
+	        });
+
+	        $('.scrollup').click(function(){
+	            $("html, body").animate({ scrollTop: 0 }, 600);
+	            return false;
+	        });
+
+	    });
+	</script>
+
+</body>
+
+</html>
+
+<!--
+
+TO-DO:
+
+* Finish About section
+* Add contact modal
+* link links
+-->
