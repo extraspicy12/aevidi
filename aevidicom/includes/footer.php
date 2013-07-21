dabc505901177603daf15b54b142bc9556add23d
Retricide <eric.segev@gmail.com>
2013-07-20 23:45:08 -0400

    Input is now throttled
    Validation will occur after .5 seconds of no typing

--- aevidicom/includes/footer.php
+++ aevidicom/includes/footer.php
@@ -122,6 +122,18 @@
 	  	$("#"+id).css("border", "1px solid green");
   	}
 
+  	function throttle(f, delay){
+    var timer = null;
+    return function(){
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = window.setTimeout(function(){
+            f.apply(context, args);
+        },
+        delay || 500);
+    };
+}
+
 	$(document).foundation();
 
 	$(document).ready(function() {
@@ -129,13 +141,13 @@
 		  $(this).foundation('section', 'reflow');
 		});
 
-		$("#contact-form input, textarea").keyup(function() {
+		$("#contact-form input, textarea").keyup(throttle(function(){
 			var validateMsg = validate(this.id, this.value);
 			if (validateMsg != "good")
 				invalidate(this.id, validateMsg);
 			else
 				makeValid(this.id);
-		});
+		}));
 
 		$("#contact-form").submit(function() {
 			var valid = true;
@@ -179,36 +191,36 @@
 		$("a.scroll").click(function(event) {
 			event.preventDefault();
 			var pos = $(this.hash).offset().top;
-			
+
 			if($(this).attr('href') == '#contact-us')
 				pos -= $(window).width() < 768 ? 0 : 65;
-				
+
 			$('html,body').animate({scrollTop:pos}, 500);
 		});
 
 	});
-	
+
 	function parallax(toggleWidth) {
 		var width = $(window).width();
-		
+
 		if(width >= toggleWidth) {
 			$.stellar({
 				horizontalScrolling: false
 			});
 		}
-			
+
 		$(window).on('resize', function() {
 			width = $(this).width();
-	
+
 			if(width < toggleWidth) {
 				$.stellar('destroy');
 			}
 			else {
 				$.stellar({
 					horizontalScrolling: false
-				});	
+				});
 			}
-		});	
+		});
 	}
   </script>
 </body>
