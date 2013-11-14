668d1ec897ab3787f57d6819dfad07b97fedc33c
Retricide <eric.segev@gmail.com>
2013-11-14 01:30:31 -0500

    Various Tweaks

--- 2dudes/wp-content/themes/twodudesfood/footer.php
+++ 2dudes/wp-content/themes/twodudesfood/footer.php
@@ -12,22 +12,22 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 	<div class="row" id="footer">
-    <div class="large-3 columns">
+    <div class="large-3 medium-6 columns">
       <?php if (!dynamic_sidebar('footer-far-left')):  ?>
       NULL
       <?php endif; ?>
     </div>
-    <div class="large-3 columns">
+    <div class="large-3 medium-6 columns">
       <?php if (!dynamic_sidebar('footer-slight-left')):  ?>
       NULL
       <?php endif; ?>
     </div>
-    <div class="large-3 columns">
+    <div class="large-3 medium-6 columns">
       <?php if (!dynamic_sidebar('footer-slight-right')):  ?>
       NULL
       <?php endif; ?>
     </div>
-    <div class="large-3 columns">
+    <div class="large-3 medium-6 columns">
       <?php if (!dynamic_sidebar('footer-far-right')):  ?>
       NULL
       <?php endif; ?>
@@ -36,10 +36,6 @@
   <div class="copyright">
     <div class="row">
 			<?php do_action( 'twodudesfood_credits' ); ?>
-			<?php printf( __( '&copy; 2013 %s', 'twodudesfood' ), '<a href="http://aeVidi.com/" rel="designer">aeVidi</a>' ); ?>
-			<span class="sep"> | </span>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twodudesfood' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
 			<?php printf( __( 'Theme for %1$s created by %2$s.', 'twodudesfood' ), '<i>Two Dudes Who Love Food</i>', '<a href="http://aeVidi.com/" rel="designer">aeVidi</a>' ); ?>
   		</div>
     </div>
